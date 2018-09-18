bdb3257a7cbc91f52fb61b3790c20c7b83454552
niklas.wantrupp <niklas.wantrupp@student.fh-kiel.de>
2018-09-18 11:43:41 +0200

    compiled new builds with gain control

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -353,7 +353,7 @@
  #define JucePlugin_ManufacturerCode       0x4d616e75 // 'Manu'
 #endif
 #ifndef  JucePlugin_PluginCode
- #define JucePlugin_PluginCode             0x4164326f // 'Ad2o'
+ #define JucePlugin_PluginCode             0x576a6379 // 'Wjcy'
 #endif
 #ifndef  JucePlugin_IsSynth
  #define JucePlugin_IsSynth                1
